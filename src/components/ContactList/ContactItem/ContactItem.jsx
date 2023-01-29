637b2caa8f293d0f3640c10bdc5a3e2739f4187d
nazinamari <93985053+nazinamari@users.noreply.github.com>
2023-01-29 17:07:15 +0000

    fix props

--- src/components/ContactList/ContactItem/ContactItem.jsx
+++ src/components/ContactList/ContactItem/ContactItem.jsx
@@ -1,15 +1,16 @@
 import PropTypes from 'prop-types';
+import { ContactItemStyle } from './ContatcItem.styled';
 
 export const ContactItem = ({ name, number, id, onClickDelete }) => {
     return (
-        <>
+        <ContactItemStyle>
             {name}: {number}
             <button type="button" onClick={() => onClickDelete(id)}>Delete</button>
-        </>
+        </ContactItemStyle>
     );
 };
 
-ContactItem.prop.Types = {
+ContactItem.propTypes = {
     id: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
     number: PropTypes.string.isRequired,
---
+++ src/components/ContactList/ContactItem/ContatcItem.styled.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const ContactItemStyle = styled.div`
+    margin-bottom: 8px;
+`;
